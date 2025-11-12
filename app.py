0bc736745812ecf1a80dd7ede8ba8896a7cab5df
Bente Bruurs <bentebruurs@acknowledge.nl>
2025-11-12 13:13:56 +0100

    site online zetten

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
- 
+import bente_app
+
 app = Flask(__name__)
  
 @app.route('/')
 def home():
     return "This is the home page"
+
+
+@app.route('/bente')
+def bente():
+    result = bente_app.krijg_output()
+    print(result)
+    return "tweede return" + result
+
+
  
- if __name__ == '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
