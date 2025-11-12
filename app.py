f442611ac7b12f3dc2c5f1d9f4dc073a1484b4d9
Bente Bruurs <bentebruurs@acknowledge.nl>
2025-11-12 11:55:06 +0100

    bente app toevoegen

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello world")
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def home():
+    return "This is the home page"
+ 
+ if __name__ == '__main__':
+    app.run(debug=True)
